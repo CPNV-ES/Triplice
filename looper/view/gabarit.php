7b68fdcff47a0e6fba8eefaed5c36ad275026f5b
Damien Jakob <damien.jakob@cpnv.ch>
2019-09-13 11:12:59 +0200

    refctor french names

--- looper/view/gabarit.php
+++ looper/view/gabarit.php
@@ -1,26 +1,16 @@
 <html>
 <<<<<<< HEAD:looper/view/gabarit.php
+=======
 <head>
     <title><?= $title ?></title>
+    <link rel="stylesheet" media="screen" type="text/css" href="./content/css/stylesheet.css"/>
 </head>
 <body>
 <div id="header">
-
+    <img id="logo" src="./content/img/looper_logo.png" alt="Logo">
 </div>
 <h1><?= $title ?></h1>
 <?= $content ?>
 </body>
-=======
-    <head>
-        <title><?= $titre ?></title>
-        <link rel="stylesheet" media="screen" type="text/css" href="./contenu/css/stylesheet.css"/>
-    </head>
-    <body>
-        <div id="header">
-            <img id="logo" src="./contenu/img/looper_logo.png" alt="Logo">
-        </div>
-        <h1><?= $titre ?></h1>
-        <?= $contenu ?>
-    </body>
 >>>>>>> master:Looper/vue/gabarit.php
 </html>
