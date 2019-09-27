039a176e15ab82e6e113acc53f8a04021b374a03
Diogo Vieira <diogo.vieira-ferreira@cpnv.ch>
2019-09-27 11:18:26 +0200

    re order all files #2

--- Looper/controller/Controller.php
+++ Looper/controller/Controller.php
@@ -1,45 +1,8 @@
 <?php
-require 'model/HomeModel.php';
-
 class Controller
 {
     static function error()
     {
         require "view/Error.php";
     }
-}
-
-function home()
-{
-    require 'view/home.php';
-}
-
-
-function create()
-{
-    require 'view/create.php';
-}
-
-function modify()
-{
-    $exerciseName = $_POST['title'];
-    $exerciseId = Database::createExercise($exerciseName);
-    require 'view/modify.php';
-}
-
-
-function manage()
-{
-    require 'view/manage.php';
-}
-
-function take()
-{
-    require 'view/take.php';
-}
-
-
-function error($message)
-{
-    require 'view/error.php';
 }
