6b588b003c826e0d2e23b0efb2006f7ce4471dc3
Diogo Vieira <diogo.vieira-ferreira@cpnv.ch>
2019-10-01 08:17:33 +0200

    css

--- Looper/Routes.php
+++ Looper/Routes.php
@@ -4,6 +4,8 @@
 Router::add("/", "HomeController@index");
 Router::add("/home", "HomeController@index");
 Router::add("/exercise/take", "ExerciceController@take");
+Router::add("/exercise/create", "ExerciceController@take");
+Router::add("/exercise/manage", "ExerciceController@take");
 Router::add("/exercise/id/modif", "ExerciceController@create");
 Router::add("/manage", "ManageController@index");
 Router::add("/error", "HomeController@error");
